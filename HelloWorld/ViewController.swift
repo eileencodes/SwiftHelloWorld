e6c496029b56053395bc1f27000008b5eace997b
eileencodes <eileencodes@gmail.com>
2017-02-05 10:39:57 -0500

    Add more buttons and phrases
    This adds more buttons and then sets up a set of phrases that can be
    used instead of a single response. Each button has 3 possible phrases
    when clicked that are chosed from a random array.
    
    The array is passed to the selectPhrase function and using the Int
    `arc4random_uniform` selects an index and then that index is used to
    select the phrase from the array.
    
    I've also consolidated the controller code into one function that can be
    called by each of the buttons.
    
    In the future I'd like to have 1 function and a case statement for the
    phrases passed to the button, but I'm not sure how yet to access the
    list of buttons from the code.

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -19,31 +19,61 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    // Adds show message function to the ViewController class
+
+    // select a random phrase from the array passed
+    func selectPhrase(phrases: Array<String>) -> String {
+        let index = Int(arc4random_uniform(UInt32(phrases.count)))
+        return phrases[index]
+    }
+
+    // shared controller code
+    func setupController(phrase: String) {
+        let alertController = UIAlertController(title: "Hello", message: phrase, preferredStyle: UIAlertControllerStyle.alert)
+
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+
+        present(alertController, animated: true, completion: nil)
+    }
+
+    // Adds showRobot function to the ViewController class
     @IBAction func showRobot() {
-        let alertController = UIAlertController(title: "Hello", message: "I am robot!", preferredStyle: UIAlertControllerStyle.alert)
-        
-        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        let arrayOfPhrases = [ "I am robot!", "001010111110101011", "Beep boop"]
 
-        present(alertController, animated: true, completion: nil)
+        setupController(phrase: selectPhrase(phrases: arrayOfPhrases))
     }
-    
+
+    // Adds showScream function to the ViewController class
     @IBAction func showScream() {
-        let alertController = UIAlertController(title: "Hello", message: "I am scream!", preferredStyle: UIAlertControllerStyle.alert)
-        
-        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
-        
-        present(alertController, animated: true, completion: nil)
+        let arrayOfPhrases = [ "I am scream!", "AHHHHHHHHHHHHHH", "Boo"]
+
+        setupController(phrase: selectPhrase(phrases: arrayOfPhrases))
     }
 
+    // Adds showBeer function to the ViewController class
     @IBAction func showBeer() {
-        let alertController = UIAlertController(title: "Hello", message: "I am beer!", preferredStyle: UIAlertControllerStyle.alert)
-        
-        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
-        
-        present(alertController, animated: true, completion: nil)
+        let arrayOfPhrases = [ "I am beer!", "mmmmmmbeeer", "Fizzle Pop Hop"]
+
+        setupController(phrase: selectPhrase(phrases: arrayOfPhrases))
     }
 
-}
+    // Adds showDog function to the ViewController class
+    @IBAction func showDog() {
+        let arrayOfPhrases = [ "I am dog!", "Woof Woof", "Doggo luv you"]
 
+        setupController(phrase: selectPhrase(phrases: arrayOfPhrases))
+    }
+
+    // Adds showEggplant function to the ViewController class
+    @IBAction func showEggplant() {
+        let arrayOfPhrases = [ "I am eggplant!", "Eggplant is puprple", "Eggplant is a vegatable"]
+
+        setupController(phrase: selectPhrase(phrases: arrayOfPhrases))
+    }
+
+    // Adds showPoop function to the ViewController class
+    @IBAction func showPoop() {
+        let arrayOfPhrases = [ "I am poop!", "Some call me Mr. Hankey", "pppptttt"]
+
+        setupController(phrase: selectPhrase(phrases: arrayOfPhrases))
+    }
+}
